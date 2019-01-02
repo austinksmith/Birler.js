fe766ea6a46394a53df9a4a251eb3873ad06c80b
asmithdev <austin@asmithdev.com>
2019-01-02 11:11:07 -0500

    Update jsdoc

--- src/birler.js
+++ src/birler.js
@@ -27,48 +27,75 @@ class birlerLogger {
     this.info = this.infoLog;
     this.warning = this.warningLog;
     this.error = this.errorLog;
-    this.errorFromThread = this.errorFromThread;
     this.saveLogEntry = this.saveToLogBook;
     this.getLogEntries = this.fetchLogBook;
     this.createAndSaveStampedMessage = this.generateTimeStampedMessage;
     this.searchLogEntries = this.searchLogBook;
   }
 
+  /**
+  * @function infoLog - Creates and saves info log event to log book with timestamp
+  * @param {string} message - Logged info event message
+  * @return {string} timeStampedMessage- Timestamped info log event record
+  */
   infoLog(message) {
     let timeStampedMessage = this.createAndSaveStampedMessage('Info', message);
     console.info(timeStampedMessage);
+    return timeStampedMessage;
   }
 
+  /**
+  * @function warningLog - Creates and saves warning log event to log book with timestamp
+  * @param {string} message - Logged warning event message
+  * @return {string} timeStampedMessage- Timestamped warning log event record
+  */
   warningLog(message) {
     let timeStampedMessage = this.createAndSaveStampedMessage('Warning', message);
     console.warn(timeStampedMessage);
+    return timeStampedMessage;
   }
 
+  /**
+  * @function errorLog - Creates and saves error log event to log book with timestamp
+  * @param {string} message - Logged error event message
+  * @param {function} reject - Optional callback function to invoke promise rejection
+  * @return {string} timeStampedMessage- Timestamped error log event record
+  */
   errorLog(message, reject) {
     let timeStampedMessage = this.createAndSaveStampedMessage('Error', message);
     console.error(timeStampedMessage);
     if(reject) {
-      reject(timeStampedMessage);
-    } else {
-      return timeStampedMessage;
+      return reject(timeStampedMessage);
     }
+    return timeStampedMessage;
   }
 
-  generateTimeStampedMessage(type, message) {
-    let record = `Birler.js v${this.version} ${type}: ${message} @ ${Date.now()}`
+  /**
+  * @function generateTimeStampedMessage - Creates and saves log event to log book with timestamp
+  * @param {string} type - Specific eventType (warning, info, error)
+  * @param {string} message - Logged event message
+  * @return {string} record - Timestamped log event record
+  */
+  generateTimeStampedMessage(eventType, message) {
+    let record = `Birler.js v${this.version} ${eventType}: ${message} @ ${Date.now()}`
     this.saveLogEntry(type.toLowerCase(), record);
     return record;
   }
 
-  errorFromThread(error, reject) {
-    let errorMessage = `#${error.lineno} in ${error.filename}: ${error.message}`;
-    this.errorLog(errorMessage, reject);
-  }
-
+  /**
+  * @function saveToLogBook - Saves log event to log book
+  * @param {string} eventType - Specific eventType to save to, (warning, info, error)
+  * @param {string} message - Logged event message
+  */
   saveToLogBook(eventType, message) {
     this.logBook[eventType].push(message);
   }
 
+  /**
+  * @function fetchLogBook - Returns saved log entries
+  * @param {string} eventType - Optional event type (info, warning, error)
+  * @return {array} logBook - Returns saved log entries
+  */
   fetchLogBook(eventType) {
     if(eventType) {
       return this.logBook[eventType];
@@ -76,6 +103,12 @@ class birlerLogger {
     return this.logBook;
   }
 
+  /**
+  * @function findStringInLogBook - Searches a specific saved log entries category (ie. info) for search string
+  * @param {array} logBookEntries - Array of log entries (ie. info log array)
+  * @param {string} searchstring - Search string to look for in logBookEntries
+  * @return {array} searchResults - Array of results matching searchString
+  */
   findStringInLogBook(logBookEntries, searchString) {
     let searchResults = [];
     let i = 0;
@@ -87,6 +120,12 @@ class birlerLogger {
     return searchResults;
   }
 
+  /**
+  * @function findStringInLogBookAllTypes - Searches all saved log entries for search string
+  * @param {object} logBook - Saved log entries
+  * @param {string} searchstring - Search string to look for in logBook
+  * @return {array} searchResults - Array of results matching searchString
+  */
   findStringInLogBookAllTypes(logBook, searchString) {
     let searchResults = [];
     let key, eventTypeResults, tmpEntries = null;
@@ -102,6 +141,12 @@ class birlerLogger {
     return searchResults;
   }
 
+  /**
+  * @function searchLogBook - Searches saved log entries for search string
+  * @param {string} searchstring - Search string to look for in logBook
+  * @param {string} eventType - Specific event type to search within (warning, error, info)
+  * @return {object} - Search results object
+  */
   searchLogBook(searchString, eventType) {
     let finalResults = [];
     if(eventType) {
